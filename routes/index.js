d9d4ae06dec3eab97ba4584d74122c6a444339fa
Mckenzie Rockwell <rockwellmckenzie@gmail.com>
2015-10-15 08:13:25 -0400

    changed nav bar, added  active indicator

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next){
-  res.render('home');
+  res.render('home', {page: "home"});
 });
 
 
 router.get('/about', function(req, res, next){
-	res.render('about');
+	res.render('about', {page: "about"});
 });
 
 router.get('/services', function(req, res, next){
-	res.render('services');
+	res.render('services', {page: "services"});
 }); 
 
 router.get('/projects', function(req, res, next){
-	res.render('projects'); 
+	res.render('projects', {page: "projects"}); 
 }); 
 
 router.get('/contact', function(req, res, next){
-	res.render('contactMe')
+	res.render('contactMe', {page: "contact"})
 });
 
 module.exports = router;
