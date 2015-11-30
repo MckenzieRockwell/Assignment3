b47bbbfb20a76bbabfb7a03b0d06980bb7a7d438
Mckenzie Rockwell <rockwellmckenzie@gmail.com>
2015-11-30 06:12:10 -0500

    todo list view and mongoose model

---
+++ models/Todo.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'), Schema = mongoose.Schema; 
+
+var todoSchema = new Schema({
+	name: {
+		type: String,
+		default: ''
+	},
+
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+
+	notes: {
+		type: String,
+		defautl: ''
+	},
+
+	completed: {
+		type: Boolean,
+		default: false
+	},
+
+	completedAt:{
+		type: Date,
+		default: ''
+	}
+
+
+});
+
+module.exports = mongoose.model('todo', todoSchema); 
