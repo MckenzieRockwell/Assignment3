ebb9e3da81ced897d70eba45b22f67b1db760f79
Mckenzie Rockwell <rockwellmckenzie@gmail.com>
2015-11-30 06:43:51 -0500

    todo routes
    added routes file for todo list, with get routes setup.

---
+++ routes/todos.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.router();
+
+var User = require('../models/user');
+
+var Todo = require('../models/todo');
+
+
+function requireAuth(req, res, next){
+	if(!req.isAuthenticated()){
+	return res.redirect('/login'); 
+	}
+	next();
+}
+
+
+
+router.get('/todolist',  requireAuth, function(req, res, next){
+	Todo.find().sort('createdAt').exec(function(err, todos){
+		if(err){
+			console.log(err);
+			res.end(err);
+		}else{
+				if(typeof todos !== 'undefined'){
+					res.render('todolist',{
+					page: 'todolist',
+					title: 'List of to do items',
+					authed: true,
+					todos: todos
+					});
+				}else{
+					res.render('todolist', {
+					page: 'todolist',
+					title: 'List of to do items',
+					authed: true
+					});
+				}
+		}
+	}); 
+});
+
+
+
+
